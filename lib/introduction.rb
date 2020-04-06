b7e905bcc9686aa64d673d265b4a6771cd5e1d0e
rosie w <rosie.wilt@gmail.com>
2020-04-06 15:40:50 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -18,30 +18,20 @@ at the end of this lesson, but try to solve these on your own first.
 
 ## Define a Method That Takes in and Uses an Parameter
 
-Define a method called `introduction` that takes in the parameter `name` and outputs
-the phrase: `"Hi, my name is #{name}."`
-
 def introduction(name)
   puts " Hi, my name is #{name}."
 end
 
-## Define a Method That Takes and Uses Two Parameters
 
-Define a method called `introduction_with_language` that takes in two parameters,
-`name` and `language` and outputs the phrase: `"Hi, my name is #{name} and I am
-learning to program in #{language}."`
 
 def introduction_with_language(name,language)
   puts `"Hi, my name is #{name} and I am learning to program in #{language}."`
+end
 
-## Define a Method With an Optional Parameter
+def introduction_with_language_optional(name,language = "Ruby")
+  puts `"Hi, my name is #{name} and I am learning to program in #{language}."`
 
-Copy the previous method and name it `introduction_with_language_optional`. It
-takes in two parameters, `name` and `language`, but the second parameter is
-optional. The default value for `language` will be "Ruby".
-
-
-
+end
 
 
 
