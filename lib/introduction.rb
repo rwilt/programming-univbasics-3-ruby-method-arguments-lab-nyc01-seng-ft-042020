30fbe3dd1412db2b955b05c35533d9093dbb077e
rosie w <rosie.wilt@gmail.com>
2020-04-06 15:45:05 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,12 +4,35 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 
+## Introduction
+=begin
+Now that we've seen a few ways of defining methods to add key functionality to
+our programs, let's try writing our own methods. You can use the tests to help
+guide you by running `learn` or `learn test` in your terminal; however, this
+will probably look familiar to you now.
+
+You'll be coding your methods in `lib/introduction.rb`. Detailed guidance is provided
+at the end of this lesson, but try to solve these on your own first.
+=end
 
 
+## Define a Method That Takes in and Uses an Parameter
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
 
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
+introduction_with_language("Rosie", "Python")
 
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+
+end
 
 
 
